0689c7aff44fc511fb1636ae1c7e6cbd7b3b355b
zgrw <ozgur.pekcagliyan@gmail.com>
2015-02-23 23:37:54 +0200

    modified comments

--- ServerApplication/include/Database Connections/DBAdapter.cpp
+++ ServerApplication/include/Database Connections/DBAdapter.cpp
@@ -2,7 +2,7 @@
  * DBAdapter.cpp
  * Main Class to manage DB operations
  * Created by: Ozgur Pekcagliyan - 2014-09-26 09:34:38 PM EEST
- * Last edited by: Ozgur Pekcagliyan - 2014-04-10
+ * Last edited by: Ozgur Pekcagliyan - 2015-02-23
  * Notes:
  * errorCode will be used to return specific error values back to user;
  * * Potential error codes are;
