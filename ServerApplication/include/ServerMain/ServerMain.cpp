f1350d7e0cc93f505463096f46bc5f8eb1ac0314
zgrw <ozgur.pekcagliyan@gmail.com>
2015-03-02 20:54:04 +0200

    Prepared for config reader

--- ServerApplication/include/ServerMain/ServerMain.cpp
+++ ServerApplication/include/ServerMain/ServerMain.cpp
@@ -44,24 +44,35 @@ void ServerMain::onConnect(const int& fd, sockaddr_in& remote)
 
 void ServerMain::onRecieve(const int& fd, unsigned char* data, const int& size)
 {
+    char buffer[512];
     //CONN_HANDLE_TYPE::iterator it = connTracker.find(fd);
     pair<sockaddr_in, int> &sec = connTracker[fd].second;
     if(sec.second == NEW) //if this is a new connection
     {
-        if(strncmp(data, HELLO_MSG, 2)) //if just searching for a server
+        if(strncmp(data, HELLO_MSG, size)) //if just searching for a server
         {
+            sprintf(buffer, "%s - %d: %s said \"HELLO\"", __FILE__, __LINE__, inet_ntoa(sec.first.sin_addr));
+            logger -> printDebugLog(buffer);
             send(fd, (unsigned char*)HELLO_MSG, 2);
+            sprintf(buffer, "%s - %d: Responded to IP %s as \"HELLO\"", __FILE__, __LINE__, inet_ntoa(sec.first.sin_addr));
+            logger -> printDebugLog(buffer);
             closeConnection(fd);
         }
-        else if(strncmp(data, INIT_MSG, 2)) //if tries to initialize a connection
+        else if(strncmp(data, INIT_MSG, size)) //if tries to initialize a connection
         {
             sec.second = INIT;
+            sprintf(buffer, "%s - %d: Initialization request from IP %s", __FILE__, __LINE__, inet_ntoa(sec.first.sin_addr));
+            logger -> printInfoLog(buffer);
             if(inControl == false)
             {
-                
+                sprintf(buffer, "%s - %d: Handshake request sent to IP %s", __FILE__, __LINE__, inet_ntoa(sec.first.sin_addr));
+                logger -> printInfoLog(buffer);
+                send(fd, (unsigned char *)HANDSHK_REQ_MSG, 2);
             }
             else
             {
+                sprintf(buffer, "%s - %d: Server is already being controlled. Illegal request from IP %s", __FILE__, __LINE__, inet_ntoa(sec.first.sin_addr));
+                logger -> printWarnLog(buffer);
                 send(fd, (unsigned char*)DENY_MSG, 2); //can't initialize, already in control
                 closeConnection(fd);
             }
@@ -69,7 +80,7 @@ void ServerMain::onRecieve(const int& fd, unsigned char* data, const int& size)
     }
     else if(sec.second == INIT)
     {
-        
+        //FIXME: Read config file and wait for right password
     }
     else
     {
